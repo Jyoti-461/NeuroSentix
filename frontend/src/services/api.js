fbd778a392aadd39d23747e3595f8606cb53b6ab
Jyoti-461 <jyotijayantofficial@gmail.com>
2026-04-10 14:54:38 +0530

    Updated API to production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  // baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://neurosentix.onrender.com",
 });
 
 export const analyzeText = async (text) => {
