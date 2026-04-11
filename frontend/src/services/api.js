0da5c4e2a18b84e3df88535ae20ae94e1d538a40
Jyoti-461 <jyotijayantofficial@gmail.com>
2026-04-12 02:45:03 +0530

    word cloud fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -15,8 +15,9 @@ export const fetchAllData = async () => {
   return response.data;
 };
 
-export const getWordCloud = () => {
-  return "http://127.0.0.1:8000/wordcloud";
+export const getWordCloud = async () => {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/wordcloud`);
+  return res;
 };
 
 export const uploadCSV = async (file) => {
